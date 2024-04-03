ba2640b7b92be6fd2091a07d60519eb50383bc93
chabin37 <7chabin@naver.com>
2024-04-03 14:44:55 +0900

    7

--- src/MyNode.java
+++ src/MyNode.java
@@ -3,19 +3,36 @@ import java.util.ArrayList;
 public class MyNode {
     private Object element;
     private MyNode parent;
-    private ArrayList children;
-
-    public MyNode() {
-        element=null;
-        parent=null;
-        children=null;
+    private ArrayList<MyNode> children;
+    MyNode() {
+        this.element = null;
+        this.parent = null;
+        this.children = new ArrayList<>();
     }
-    MyNode(Object e){element=e;}
-    public Object element(){return element;}
-    public MyNode parent(){return parent;}
-    public ArrayList children(){return children;}
-    public int degree(){return this.children.size();}
-    public void setElement(Object e){this.element=e;}
-    public void setParent(MyNode p){this.parent=p;}
-    public void setChildren(ArrayList c){this.children=c;}
+    MyNode(Object e) {
+        this.element = e;
+        this.parent = null;
+        this.children = new ArrayList<>();
+    }
+    public Object element() {
+        return this.element;
+    }
+    public MyNode parent() {
+        return this.parent;
+    }
+    public ArrayList<MyNode> children() {
+        return this.children;
+    }
+    public int degree() {
+        return this.children.size();
+    }
+    public void setElement(Object e) {
+        this.element = e;
+    }
+    public void setParent(MyNode p) {
+        this.parent = p;
+    }
+    public void setChildren(ArrayList<MyNode> c) {
+        this.children = c;
+    }
 }
--- src/MyTree.java
+++ src/MyTree.java
@@ -7,12 +7,27 @@ public class MyTree {
         root=new MyNode(e);
         root.setChildren(new ArrayList());
     }
-    public int size(){}
-    public MyNode root() {}
-    public ArrayList children(MyNode v) {}
-    public boolean isExternal(MyNode v) {}
-    public MyNode addRoot(Object e) {}
-    public MyNode addNode(Object e) {}
+    public int size(){}//자신을 포함한 모든 자식 노드의 갯수(트리 크기)
+    public MyNode root() {return root;}
+    public ArrayList children(MyNode v) {
+        return v.children();
+    }
+    public boolean isExternal(MyNode v) {
+       return v.children().isEmpty();
+    }
+    public MyNode addRoot(Object e) {
+        MyNode mn=new MyNode(e);
+        var arr=new ArrayList();
+        arr.add(new MyNode(this.root.element()));
+        mn.setChildren(arr);
+        this.root=mn;
+        return mn;
+    }
+    public MyNode addNode(Object e) {
+        MyNode mn=new MyNode(e);
+        this.root.children().add(mn);
+        return root;
+    }
     public MyNode addChild(MyNode v, Object e) {}
     public MyNode addChild(MyNode v, int i, Object e) {}
     public MyNode setChild(MyNode v, int i, Object e) {}
