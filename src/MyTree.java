c250293a54734519bb1984d830db54844f9c834b
chabin37 <7chabin@naver.com>
2024-04-04 12:51:54 +0900

    1

--- src/MyTree.java
+++ src/MyTree.java
@@ -7,7 +7,18 @@ public class MyTree {
         root=new MyNode(e);
         root.setChildren(new ArrayList());
     }
-    public int size(){}//자신을 포함한 모든 자식 노드의 갯수(트리 크기)
+    public int size(){
+        int result=1;
+        result+=size(this.root);
+        return result;
+    }
+    private int size(MyNode v){
+        int temp=1;
+        for(MyNode m:v.children()){//children이 없을경우, 1만 반환=>총 size 알 수 있음
+            temp+=size(m);
+        }
+        return temp;
+    }
     public MyNode root() {return root;}
     public ArrayList children(MyNode v) {
         return v.children();
@@ -28,8 +39,20 @@ public class MyTree {
         this.root.children().add(mn);
         return root;
     }
-    public MyNode addChild(MyNode v, Object e) {}
-    public MyNode addChild(MyNode v, int i, Object e) {}
-    public MyNode setChild(MyNode v, int i, Object e) {}
-    public MyNode removeChild(MyNode v, int i) {}
+    public MyNode addChild(MyNode v, Object e) {
+        v.children().add(new MyNode(e));
+        return v;
+    }
+    public MyNode addChild(MyNode v, int i, Object e) {
+        v.children().add(i,new MyNode(e));
+        return v;
+    }
+    public MyNode setChild(MyNode v, int i, Object e) {
+        v.children().set(i,new MyNode(e));
+        return v;
+    }
+    public MyNode removeChild(MyNode v, int i) {
+        v.children().remove(i);
+        return v;
+    }
 }
