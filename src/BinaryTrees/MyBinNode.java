6e943cb17fd3498336cd9ff4be45b61cc93f2cb1
chabin37 <7chabin@naver.com>
2024-04-07 02:10:19 +0900

    1

--- src/BinaryTrees/MyBinNode.java
+++ src/BinaryTrees/MyBinNode.java
@@ -1,4 +1,15 @@
 package BinaryTrees;
 
 public class MyBinNode extends MyNode{
+
+    MyBinNode(){
+        super();
+    }
+    MyBinNode(Object e){
+        super(e);
+    }
+    public MyBinNode left() {  }
+    public MyBinNode right() {  }
+    public void setLeft(MyBinNode v) {  }
+    public void setRight(MyBinNode v) {  }
 }
